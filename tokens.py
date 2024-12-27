26f2de6786b7a2cf664b40a43aab4c8a31c74512
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-12-26 22:33:45 -0500

    adding support for variables

--- tokens.py
+++ tokens.py
@@ -1,9 +1,26 @@
 class Tokens:
     def __init__(self):
-        self.recognized_tokens = {"preach" : self.print_token}
+        self.recognized_tokens = {
+            "preach" : self.print_token,
+            "ts" : self.make_variable
+            }
 
     def print_token(self, args):
         for arg in args:
             if not type(arg) == str:
                 raise Exception("oops, argument not of type string!")
+
+            if arg in self.recognized_tokens:
+                arg = self.recognized_tokens[arg]
+
             print(arg, end=' ')
+        print('\n')
+    
+    def make_variable(self, args):
+        if len(args) != 2:
+            raise Exception("ts can only have two arguments")
+        
+        var = args[0]
+        val = args[1]
+
+        self.recognized_tokens[var] = val
