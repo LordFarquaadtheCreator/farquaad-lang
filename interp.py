ce1410c2877483af59efa644325e4ae70187c521
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-12-26 22:02:12 -0500

    constructor now reads file contents

--- interp.py
+++ interp.py
@@ -1,9 +1,22 @@
-import Interp
-import sys
+class Interp:
+    def __init__(self, file_path):
+        self.path = file_path
+        self.contents = None
+        self.get_file(file_path)
+
+    '''
+    opens and loads all file `path` contents to self.contents
+    '''
+    def get_file(self, path: str) -> None:
+        import os
+        try:
+            file = open(path, "r")
+            self.contents = file.read()
+            file.close()
+        except Exception as e:
+            print("Ruh roh raggy, smth went wrong!", e)
+            exit(1)
 
 if __name__ == "__main__":
-    try:
-        file = sys.argv[1]
-        i = Interp(file)
-    except Exception as e:
-        print(f"AOOOWGA! Error spotted on line {e}. CRASHING OUT!!!")
+    i = Interp("test.skibidi")
+    
